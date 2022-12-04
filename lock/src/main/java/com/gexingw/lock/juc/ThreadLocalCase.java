1ba213fda1bb012fa5a05738541b90164ee81d88
W.sf <wolfs_9@hotmail.com>
2022-12-04 23:05:21 +0800

    :sparkles: 更新threadlocal的案例

--- lock/src/main/java/com/gexingw/lock/juc/ThreadLocalCase.java
+++ lock/src/main/java/com/gexingw/lock/juc/ThreadLocalCase.java
@@ -12,14 +12,13 @@ public class ThreadLocalCase {
     public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
     public static void main(String[] args) throws InterruptedException {
+
+        threadLocal.set("Main-thread");
         Thread thread1 = new Thread("thread-1"){
             @Override
             public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(threadLocal.get());
+
                 threadLocal.set("thread-1");
 
                 System.out.println(Thread.currentThread().getName() + threadLocal.get());
@@ -29,11 +28,8 @@ public class ThreadLocalCase {
         Thread thread2 = new Thread("thread-2"){
             @Override
             public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(threadLocal.get());
+
                 threadLocal.set("thread-2");
 
                 System.out.println(Thread.currentThread().getName() + threadLocal.get());
@@ -43,11 +39,7 @@ public class ThreadLocalCase {
         Thread thread3 = new Thread("thread-3"){
             @Override
             public void run() {
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(threadLocal.get());
 
                 threadLocal.set("thread-3");
 
