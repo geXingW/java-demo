c8b663cdfe87eadd526917a0f49d963916bea1a7
GeXingW <wolfs_9@hotmail.com>
2024-05-25 11:56:27 +0800

    :sparkles: 冒泡排序调整

--- sort-alg/src/main/java/com/gexingw/sort/BubbleSort.java
+++ sort-alg/src/main/java/com/gexingw/sort/BubbleSort.java
@@ -8,19 +8,20 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] unSort = {2, 6, 3, 7, 4, 9, 10, 1};
+        int[] unSort = {2, 6, 3, 7, 1, 4, 9, 10, 5, 8};
+//        int[] unSort = {2, 6, 3, 7};
         for (int i = 0; i < unSort.length; i++) {
-            for (int j = i + 1; j < unSort.length; j++) {
-
-                if (unSort[i] > unSort[j]) {
-                    int temp = unSort[i];
-                    unSort[i] = unSort[j];
-                    unSort[j] = temp;
+            for (int j = 0; j < unSort.length - i - 1; j++) {
+                if (unSort[j] > unSort[j + 1]) {
+                    int temp = unSort[j];
+                    unSort[j] = unSort[j + 1];
+                    unSort[j + 1] = temp;
                 }
+//                System.out.println(Arrays.toString(unSort));
 //                System.out.println("i=" + i + ",j=" + j);
             }
             System.out.println(Arrays.toString(unSort));
-
+            System.out.println();
         }
 
 //        System.out.println(Arrays.toString(unSort));
